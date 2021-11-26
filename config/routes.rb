f8be9eeaafcb3afe22576d8462fa8fd1b20867ea
Deng <donggdeng@gmail.com>
2021-11-26 20:11:59 +0800

    add routes for admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+
   devise_for :users
   resources :events
 
+  namespace :admin do
+    root "events#index"
+    resources :events
+  end
+  
   root "events#index"
 end
