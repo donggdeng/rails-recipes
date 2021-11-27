247589b72aac430a34366b2f467f9002529b70fc
Deng <donggdeng@gmail.com>
2021-11-27 15:18:02 +0800

    add ticket for event

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_27_064319) do
+ActiveRecord::Schema.define(version: 2021_11_27_070739) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -58,6 +58,16 @@ ActiveRecord::Schema.define(version: 2021_11_27_064319) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_profiles_on_user_id"
+  end
+
+  create_table "tickets", force: :cascade do |t|
+    t.integer "event_id"
+    t.string "name"
+    t.text "description"
+    t.integer "price"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["event_id"], name: "index_tickets_on_event_id"
   end
 
   create_table "users", force: :cascade do |t|
